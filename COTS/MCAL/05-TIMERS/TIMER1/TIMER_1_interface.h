4c8385139614b418a614572d34a4e20654d29ceb
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-09-09 16:46:41 +0300

    Add files via upload

--- COTS/MCAL/05-TIMERS/TIMER1/TIMER_1_interface.h
+++ COTS/MCAL/05-TIMERS/TIMER1/TIMER_1_interface.h
@@ -1,6 +1,6 @@
 /********************************************************/
-/********** Name    : Mahmoud a raouf mahmoud ***********/
-/********** Date    : 15/11/2022              ***********/
+/********** Name    : Ahmed Bakr			  ***********/
+/********** Date    : 9/9/2023	              ***********/
 /********** SWC     : TIMER1                  ***********/
 /********** Version : 1.0                     ***********/
 /********************************************************/
@@ -9,33 +9,54 @@
 
 /************************  Sec: Includes ******************************/
 /*lIB*/
-#include "ERROR_STATE.h"
-#include "STD_TYPES.h"
-#include "BIT_MATH.h"
+
+#include "../../../LIB/STD_TYPES.h"
+#include "../../../LIB/BIT_MATH.h"
 /*MCAL*/
-#include "TIMER_1_interface.h"
+
 #include "TIMER_1_private.h"
 #include "TIMER_1_config.h"
+#include "../../01-DIO/DIO_interface.h"
 
 /************************  Sec: MACROS ******************************/
 /**<_prescaler*/
-#define PRESCALAR_DIV_BY_1      0
-#define PRESCALAR_DIV_BY_8      1
-#define PRESCALAR_DIV_BY_64     2   
-#define PRESCALAR_DIV_BY_256    3
-#define PRESCALAR_DIV_BY_1024   4
+#define PRESCALAR_DIV_BY_1 0
+#define PRESCALAR_DIV_BY_8 1
+#define PRESCALAR_DIV_BY_64 2
+#define PRESCALAR_DIV_BY_256 3
+#define PRESCALAR_DIV_BY_1024 4
+#define EXTERNAL_CLK_SRC_FALLING 5
+#define EXTERNAL_CLK_SRC_RISING 6
 
+/**<_modes*/
+#define INPUT_CAPTURE_MODE 0
+#define OUTPUT_CAPTURE_MODE 1
+#define PWM_MODE 2
+#define COUNTER_MODE 3
+
+/**<_edge*/
+#define RISING_EDGE 0
+#define FALLING_EDGE 1
 
 /************************  Sec: MACRO like Function ******************************/
+#define TIMER1_DISABLE_WGM_OCR() (TIMER1_U8_TCCR1A_REG = 0) // Disable WGM & OCR
+
+#define TIMER1_CLEAR_INPUT_CAPTURE_FLAG() (CLR_BIT(TIMER1_U16_TIFR_REG, 5)) // Clear ICF1
+
+#define TIMER1_CLEAR_COUNTER_REG() (do { \
+	TIMER1_U8_TCNT1H_REG = 0;            \
+	TIMER1_U8_TCNT1L_REG = 0;            \
+} while (0))
 
 /************************  Sec: User Defined Data Types ******************************/
-typedef struct 
-{
-    u8 prescalar    :3;
-    u8 timer_mode   :2;
-    u8 
-} Timer1_t;
 
+typedef struct
+{
+	u8 prescalar : 3;
+	u8 timer_mode : 2;
+	u8 edge : 1;
+	u8 : 2;
+} Timer1_t;
 
 /************************  Sec: Software Interfaces ******************************/
 
@@ -60,26 +81,25 @@ Std_ReturnType Timer1_init(const Timer1_t *_timer);
 Std_ReturnType Timer1_deinit(const Timer1_t *_timer);
 
 /**
- * @brief 
- * 
+ * @brief
+ *
  * @param _timer1 pointer to timer1 module configurations @ref Timer1_t
  * @param _value  value to be written to Timer1 register
  * @return Std_ReturnType
  *          (E_OK) : The function done successfully
- *          (E_NOT_OK) : The function has issue while performing this action 
+ *          (E_NOT_OK) : The function has issue while performing this action
  */
 Std_ReturnType Timer1_write(const Timer1_t *_timer, u16 _value);
 
 /**
- * @brief 
- * 
+ * @brief
+ *
  * @param _timer1 pointer to timer1 module configurations @ref Timer1_t
  * @param _value  pointer to value to be read from Timer1 register
  * @return Std_ReturnType
  *          (E_OK) : The function done successfully
- *          (E_NOT_OK) : The function has issue while performing this action 
+ *          (E_NOT_OK) : The function has issue while performing this action
  */
-Std_ReturnType Timer1_read(const Timer1_t *_timer, u16 *_value);
+Std_ReturnType Timer1_read(const Timer1_t *_timer, u16 *_value, u16 *_freq, u8 *_duty_cycle);
 
-
-#endif /**< __TIMER_1_INTERFACE_H__*/
+#endif /**<__TIMER_1_INTERFACE_H__*/
--- COTS/MCAL/05-TIMERS/TIMER1/TIMER_1_program.c
+++ COTS/MCAL/05-TIMERS/TIMER1/TIMER_1_program.c
@@ -9,7 +9,9 @@
 
 #include "TIMER_1_interface.h"
 
-static void Timer1_setPrescaler(Timer1_t *_timer);
+/**<_Helper Functions*/
+static void Timer1_setPrescaler(const Timer1_t *_timer);
+static void Timer1_edgeSelect(const Timer1_t *_timer);
 
 /**
  * @brief initializes timer1 module in input capture mode
@@ -29,7 +31,33 @@ Std_ReturnType Timer1_init(const Timer1_t *_timer)
 	}
 	else
 	{
+		DIO_voidInit();
+		TIMER1_CLEAR_INPUT_CAPTURE_FLAG();						// Clear Input Capture Flag
+		Timer1_setPrescaler(_timer); 							// Set Timer1 CLK Prescalar 
+		switch (_timer->timer_mode)
+		{
+		case INPUT_CAPTURE_MODE:
+			/* code */
+			DIO_SetPinDirection(DIO_PORTD, DIO_PIN6, DIO_INPUT); 	// Set D6 (ICP1) as input
+			TIMER1_DISABLE_WGM_OCR(); 								// Disable WGM & OCR
+			Timer1_edgeSelect(_timer);								// Select edge
+			break;
+
+		case OUTPUT_CAPTURE_MODE:
+			/* code */
+			break;
+
+		case PWM_MODE:
+			/* code */
+			break;	
 		
+		case COUNTER_MODE:
+			/* code */
+			break;	
+		
+		default:
+			break;
+		}
 	}
 
 	return ret;
@@ -85,54 +113,68 @@ Std_ReturnType Timer1_write(const Timer1_t *_timer, u16 _value)
 /**
  * @brief
  *
- * @param _timer1 pointer to timer1 module configurations @ref Timer1_t
- * @param _value  pointer to value to be read from Timer1 register
+ * @param _timer1 		pointer to timer1 module configurations @ref Timer1_t
+ * @param _value  		pointer to value to be read from Timer1 register
+ * @param _freq	  		pointer to value calculated for the frequency of the input signal
+ * @param _duty_cycle   pointer to value calculated for the duty cycle of the input signal
  * @return Std_ReturnType
  *          (E_OK) : The function done successfully
  *          (E_NOT_OK) : The function has issue while performing this action
  */
-Std_ReturnType Timer1_read(const Timer1_t *_timer, u16 *_value)
+Std_ReturnType Timer1_read(const Timer1_t *_timer, u16 *_value, u16 *_freq, u8 *_duty_cycle)
 {
 	Std_ReturnType ret = E_OK;
 
-	if ((NULL == _timer) || (NULL == _value))
+	if ((NULL == _timer) || (NULL == _value) || (NULL == _freq) || (NULL == _duty_cycle))
 	{
 		ret = E_NOT_OK;
 	}
 	else
 	{
+		TIMER1_CLEAR_INPUT_CAPTURE_FLAG();
+		TIMER1_U8_TCNT1H_REG = 0;
+		TIMER1_U8_TCNT1L_REG = 0;
 	}
 
 	return ret;
 }
 
-static void Timer1_setPrescaler(Timer1_t *_timer){
+static void Timer1_setPrescaler(const Timer1_t *_timer){
 	switch (_timer->prescalar)
 	{
 	
 	case PRESCALAR_DIV_BY_1:
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 0);
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 1);
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
 
 	case PRESCALAR_DIV_BY_8:
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 0);
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 1);
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
 	
 	case PRESCALAR_DIV_BY_64:
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 0);
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 1);
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
 
 	case PRESCALAR_DIV_BY_256:
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 0);
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 1);
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
 
 	case PRESCALAR_DIV_BY_1024:
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 0);
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 1);
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
 
 	case EXTERNAL_CLK_SRC_FALLING:
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 0);
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 1);
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
@@ -143,6 +185,25 @@ static void Timer1_setPrescaler(Timer1_t *_timer){
 		SET_BIT(TIMER1_U8_TCCR1B_REG, 2);
 		break;
 
+	default:
+		break;
+	}
+}
+
+static void Timer1_edgeSelect(const Timer1_t *_timer)
+{
+	switch (_timer->edge)
+	{
+	case RISING_EDGE:
+		/* code */
+		SET_BIT(TIMER1_U8_TCCR1B_REG, 6);
+		break;
+
+	case FALLING_EDGE:
+		/* code */
+		CLR_BIT(TIMER1_U8_TCCR1B_REG, 6);
+		break;
+	
 	default:
 		break;
 	}
